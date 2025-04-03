8ca9a4484a376c6f38a6f77af5ed1606ee3fb9cd
sachin barali <baralisachin5925@gmail.com>
2025-04-03 22:09:37 +0545

    feat: Reset password, Register User and Edit profile feature - Reset password using django reset password features and sending reset password link in email but its done in console - Register the user - Allowed user to edit the profile which contains email, firstname,lastname,profile and date_of_birth

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,41 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from .models import Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label = 'Repeat password',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username','first_name','email']
+
+    
+    # validating password and password2 fields
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+    
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name','last_name','email']
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth','photo']
--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm,UserRegistrationForm,UserEditForm,ProfileEditForm
 from django.contrib.auth import authenticate,login
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 # Create your views here.
@@ -27,6 +28,30 @@ def user_login(request):
     return render(request,'account/login.html',{'form':form})
 
 
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            new_user.save()
+            Profile.objects.create(user = new_user)
+            return render(
+                request,
+                'account/register_done.html',
+                {'new_user':new_user}
+            )
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        'account/register.html',
+        {'user_form':user_form}
+    )
+
+
 @login_required
 def dashboard(request):
     return render(
@@ -36,4 +61,25 @@ def dashboard(request):
             'section': 'dashboard',
         }
     )
-                  
+
+@login_required
+def edit(request):
+    profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,data=request.POST)
+        profile_form = ProfileEditForm(instance=profile,data=request.POST,files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=profile)
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form':user_form,
+            'profile_form':profile_form,
+        }
+    )
