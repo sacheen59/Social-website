df132869bc1e7ff3359af5396e7485577b9a76c7
sachin barali <baralisachin5925@gmail.com>
2025-04-04 01:49:30 +0545

    feat: social auth
    - social-auth packages added.
    - serving our site under a domain name in our local machine

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jt@0zl9xumbtqm-99(nbcughq4@%8%n825ueiwxh#$2-wcyxrg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com','localhost','127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
